ac6268db18d7b66c18662eba4956d97458a7cbd4
Jared Sackey <jared.sackey@student.rocva.nl>
2020-03-17 15:38:55 +0100

    klaar

--- opdracht01/js/script.js
+++ opdracht01/js/script.js
@@ -3,11 +3,6 @@ var maybe = "oranje";
 var go = "groen";
 
 window.onload = function(){
-    
-
-    document.getElementById("light-top").style.backgroundColor = "red";
-    document.getElementById("light-middle").style.backgroundColor = "orange";
-    document.getElementById("light-bottom").style.backgroundColor = "green";
 
     document.getElementById("light-top").innerHTML = stop;
     document.getElementById("light-middle").innerHTML = maybe;
